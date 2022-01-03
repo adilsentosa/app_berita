85a86ef09e52087e2601e8f1858563fe6b928135
adilsentosa <mohamatadilsentosa@gmail.com>
2022-01-03 11:03:08 +0800

    frist commit

---
+++ lib/detail.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class  extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(children:<Widget> [
+        Container(
+          height: 250,
+          color: Colors.amber,
+        )
+      ],)
+    );
+  }
+}
