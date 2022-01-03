72c45a2eab9c641b8b2606fa8c66fecdaa16e6ac
adilsentosa <mohamatadilsentosa@gmail.com>
2022-01-03 12:22:47 +0800

    fix home and details

--- lib/detail.dart
+++ lib/detail.dart
@@ -4,35 +4,40 @@ class detail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: <Widget>[
-        Container(
-          height: 250,
-          color: Colors.grey[200],
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'title',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'date',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 5),
-              Text('content'),
-              Divider(),
-              Text('penulis :'),
-              Text('sumber'),
-            ],
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 250,
+            color: Colors.grey[200],
           ),
-        )
-      ],
-    ));
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'title',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'date',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 5),
+                Text('content'),
+                Divider(),
+                Text('penulis :'),
+                Text('sumber'),
+              ],
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.close),
+        onPressed: () => Navigator.pop(context),
+      ),
+    );
   }
 }
